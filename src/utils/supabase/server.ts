c770473b7677f657ef58f10b55cd50514c46f384
enamitomotaro <0519tomotaro@gmail.com>
2025-06-15 19:29:01 +0900

    fix: cookie 操作を非同期に変更

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -8,14 +8,14 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
+        async get(name: string) {
+          return (await cookieStore).get(name)?.value
         },
-        set(name: string, value: string, options: any) {
-          cookieStore.set({ name, value, ...options })
+        async set(name: string, value: string, options: any) {
+          (await cookieStore).set({ name, value, ...options })
         },
-        remove(name: string, options: any) {
-          cookieStore.delete({ name, ...options })
+        async remove(name: string, options: any) {
+          (await cookieStore).delete({ name, ...options })
         },
       },
     }
